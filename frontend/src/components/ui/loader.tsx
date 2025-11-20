1348e5af83e99330b4db7cf76e8557c7d5879720
unknown <samuel.imex@gmail.com>
2025-11-20 15:07:54 +0100

    clean up code

--- frontend/src/components/ui/loader.tsx
+++ frontend/src/components/ui/loader.tsx
@@ -1,12 +1,3 @@
-// export const Loader = () => {
-//   return (
-//     <div className="flex justify-center items-center py-10">
-//       <div className="lds-ellipsis">
-//         <div></div><div></div><div></div><div></div>
-//       </div>
-//     </div>
-//   );
-// };
 import React from "react";
 
 interface LoaderProps {
